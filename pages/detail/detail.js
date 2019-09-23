0630ea48193106221ea7278464db59e86f1567d4
unknown <775071305@qq.com>
2019-09-23 14:40:43 +0800

    新增每日一句详情

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,21 +1,12 @@
-// pages/test/test.js
+// pages/detail/detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  imgUrls: [
-    'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-    'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-    'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-  ],
-  indicatorDots: false,
-  autoplay: false,
-  interval: 5000,
-  duration: 1000,
-  wxWidth:wx.getSystemInfoSync().windowWeight,
-  dsapi:''
+ dsapi:'',
+ wxWidth:wx.getSystemInfoSync().windowWidth+"px",
   },
 
   /**
@@ -37,30 +28,7 @@ Page({
       }
       })
   },
-  search: function (value) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
-        }, 200)
-    })
-},
 
-selectResult: function (e) {
-    console.log('select result', e.detail)
-},
-playaudio:function(e){
-  console.log(1)
-  const innerAudioContext = wx.createInnerAudioContext()
-  innerAudioContext.autoplay = true
-  innerAudioContext.src = e.currentTarget.dataset['src']
-  innerAudioContext.onPlay(() => {
-    console.log('开始播放')
-  })
-  innerAudioContext.onError((res) => {
-    console.log(res.errMsg)
-    console.log(res.errCode)
-  })
-},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
